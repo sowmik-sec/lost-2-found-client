4d7286b534ae5121425fbe55b8cbcc816a9ff2c3
sowmik-sec <sowmik.sec@gmail.com>
2025-05-20 21:01:26 +0600

    create form

---
+++ src/components/Shared/Forms/LForm.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
+
+type TFormConfig = {
+  resolver?: any;
+  defaultValues?: Record<string, unknown>;
+};
+
+type TFormProps = {
+  children: React.ReactNode;
+  onSubmit: SubmitHandler<FieldValues>;
+} & TFormConfig;
+
+const LForm = ({ children, onSubmit, resolver, defaultValues }: TFormProps) => {
+  const formConfig: TFormConfig = {};
+  if (resolver) {
+    formConfig["resolver"] = resolver;
+  }
+  if (defaultValues) {
+    formConfig["defaultValues"] = defaultValues;
+  }
+  const methods = useForm(formConfig);
+  const { handleSubmit, reset } = methods;
+  const submit: SubmitHandler<FieldValues> = (data) => {
+    onSubmit(data);
+    reset();
+  };
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(submit)}>{children}</form>
+    </FormProvider>
+  );
+};
+
+export default LForm;
