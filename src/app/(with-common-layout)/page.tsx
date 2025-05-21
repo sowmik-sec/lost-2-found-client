a0278fd53c82b0eb8837c17a03a4dc27ba5df7b4
sowmik-sec <sowmik.sec@gmail.com>
2025-05-21 22:52:25 +0600

    home page

--- src/app/(with-common-layout)/page.tsx
+++ src/app/(with-common-layout)/page.tsx
@@ -1,8 +1,12 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 const Home = () => {
-  return <Box>This is home page</Box>;
+  return (
+    <Box>
+      <Typography variant="h1">This is home page in bigger form</Typography>
+    </Box>
+  );
 };
 
 export default Home;
